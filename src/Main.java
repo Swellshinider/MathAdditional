8aa6506c105a5786eb9aa4942bf7f97d9171bffa
OnlyEduardo <eduardoleal.contact@gmail.com>
2020-10-21 16:00:11 -0300

    new branch

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import com.mathAdditional.Exceptions.InvalidValue;
+import com.mathAdditional.SecondDegreeFunction;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws InvalidValue {
+
+        double a = 1;
+        double b = 9;
+        double c = -10;
+
+        SecondDegreeFunction sdf = new SecondDegreeFunction(a, b, c);
+
+        System.out.println(sdf);
+    }
+}
